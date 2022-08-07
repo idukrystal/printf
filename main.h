a87f6456ed0fd77561cc9000c76bcd613f37ef71
Meklita <mektnz@gmail.com>
2022-08-07 05:55:26 -0700

    main

--- main.h
+++ main.h
@@ -4,9 +4,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+
 int _printf(const char *format, ...);
 unsigned int validate(const char *, int);
 int is_valid(char c);
 char *convert_char(void *);
+
 
 #endif /* MAIN_H  */
