df221bdbfe35d793f78b753857079b6f69134f21
crysart <idukrystal@gmail.com>
2022-08-07 11:12:24 +0100

    task o

--- main.h
+++ main.h
@@ -8,6 +8,5 @@ int _printf(const char *format, ...);
 unsigned int validate(const char *, int);
 int is_valid(char c);
 char *convert_char(void *);
-char (*get_conversion_function(void *))(char *);
 
 #endif /* PRINTF_H */
