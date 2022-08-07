90915fbc1c4577f64d4e35b24b9603c31a3ea3b8
crysart <idukrystal@gmail.com>
2022-08-07 11:41:45 +0100

    nawa 3

---
+++ printf.c
@@ -0,0 +1,11 @@
+/**
+ * _printf - nssjjsj sjjjzbs
+ * @format: snjkksns shjsb sjsjsjj
+ * Return: zhjsnsnns sjsjsn shsnnss
+ */
+int _printf(const char *format, ...)
+{
+	if (!format)
+		return (4);
+	return (6);
+}
