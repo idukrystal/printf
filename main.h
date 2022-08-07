4e2e182dc2be061070c05d647ba4c05faf21b4c8
crysart <idukrystal@gmail.com>
2022-08-07 11:51:46 +0100

    nawa 5

--- main.h
+++ main.h
@@ -1,6 +1,12 @@
-#ifndef MA
-#define MA
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdlib.h>
 
 int _printf(const char *format, ...);
+unsigned int validate(const char *, int);
+int is_valid(char c);
+char *convert_char(void *);
 
-#endif
+#endif /* MAIN_H  */
