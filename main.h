4cf94f399d8e959cf19b09b27bd80a05cd07f127
Meklita <mektnz@gmail.com>
2022-08-08 08:46:50 -0700

    main

--- main.h
+++ main.h
@@ -13,5 +13,6 @@ int print_specifier(char c, va_list args);
 unsigned int  print_number(int n);
 int _putchar(int );
 int _puts(char *);
+unsigned int to_base(unsigned int n, charb);
 
 #endif /* MAIN_H  */
