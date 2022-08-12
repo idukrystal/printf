b122052f5bbf77b411ebe9ab6622c81b92c7fbb3
Meklita <mektnz@gmail.com>
2022-08-12 13:26:46 -0700

    commit

--- width.c
+++ width.c
@@ -2,11 +2,38 @@
 
 /**
  * width - get the widh
- *
+ * @format:
+ * @list:
+ * @i:
  *
  * Return: the width
  */
 
-int width()
+int width(const char *format, int *i, va_list list)
 {
+	int j, w;
+
+	w = 0;
+
+	for (j = *i + 1; format[j] != '\0'; j++)
+	{
+		if (is_digit(format[j]))
+		{
+			w *= 10;
+			w += format[j] - '0';
+		}
+		else if (format[j] == '*')
+		{
+			j++;
+			w = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+	*i = j - 1;
+	return (0);
+}
+
+
 
