20ba3a7a81c94c48e75b1df54c52bfa24b276cc6
crysart <idukrystal@gmail.com>
2022-08-12 12:08:22 +0100

    p

--- convert_function.c
+++ convert_function.c
@@ -12,8 +12,13 @@ int to_base(unsigned int n, char b, flags *f)
 {
 	unsigned int i = 0;
 
-	if ((f->hash == 1 && n != 0) && (b == 'X' || b == 'x'))
-		i += _putchar('0') + _putchar(b);
+	if (f->hash == 1 && n != 0)
+	{
+		if (b == 'X' || b == 'x')
+			i += _putchar('0') + _putchar(b);
+		if (b == 'o')
+			i += _putchar('0');
+	}
 	i += base_n(n, b);
 	return (i);
 }
